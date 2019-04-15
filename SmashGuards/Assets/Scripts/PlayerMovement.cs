4e67d54db115ca06be1b6526a8cc9431158da2e3
Brad <brad.power@gmail.com>
2019-04-15 10:15:39 +0800

    Initial Commit

---
+++ SmashGuards/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float moveSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector3(h, 0, v) * moveSpeed;
+
+    }
+}
